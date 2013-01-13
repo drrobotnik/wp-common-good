95c74aa5a04313bc51662443c513e656054b7420
Brandon <brandon.lavigne@gmail.com>
2013-01-12 19:11:02 -0800

    initial commit.

---
+++ wp-common-good.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+Plugin Name: WP Common Good
+Plugin URI: http://www.caavadesign.com
+Description: Common good is an attempt to create a WP knowledge base of common issues within a wordpress site. Part support ticketting, part help desk.
+Version: 0.1
+Author: Brandon Lavigne
+Author URI: http://www.caavadesign.com
+*/
+
+/**
+ * Copyright (c) 2013 Brandon Lavigne. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
+require_once('rpc.php');
